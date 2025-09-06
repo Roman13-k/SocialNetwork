148897b53154cf9cc71183b3eb6e1fff99da4157
Roman13-k <romank050607@gmail.com>
2025-09-07 00:16:29 +0300

    not-found and layout fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,16 @@
-import "../globals.css";
 import Footer from "@/components/ui/layout/footer/Footer";
 import Header from "@/components/ui/layout/header/Header";
+import { H1 } from "@/components/ui/shared/text/H";
+import React from "react";
 
-export default function LayoutWithFooter({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function notFound() {
   return (
     <div className='flex flex-col relative w-full'>
       <div className='flex w-full'>
         <Header />
-        <main className='w-full'>{children}</main>
+        <main className='w-full flex justify-center items-center'>
+          <H1>This page is not found</H1>
+        </main>
       </div>
       <Footer />
     </div>
